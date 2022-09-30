b1e55fe0d122a1c043f69b4fac71f001449ac98f
SantaAnnaUTH <114435071+SantaAnnaUTH@users.noreply.github.com>
2022-09-30 00:32:10 -0500

    Add files via upload

---
+++ CV personal/index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/irene.css">
+	<link rel="icon" type="image/x-icon" href="image/irene.png">
+	<!--><link rel="stylesheet" href="CV personal/a.css"></!-->
+	<title>Curriculum Vitae</title>
+</head>
+<body>
+	<header>
+		<div class="contenedor1">
+		<h1>CV Personal</h1>
+		</div>
+	</header>
+
+	<nav>
+		<div class="contenedor">
+			<ul>
+				<li><a href="#ancla-1">Sobre Mi</a></li>
+				<li><a href="#ancla-2">Estudios</a></li>
+				<li><a href="#ancla-3">Portafolio</a></li>
+				<li><a href="#ancla-4">Intereses</a></li>
+				<li><a href="#ancla-5">Contato</a></li>
+				
+			</ul>
+		</div>
+	</nav>
+	<div id="presentacion">
+		<div class="contenedor2">
+			<h2><a name="ancla-1">JOSÉ FERNANDO URIZA VALLEJO</a></h2>
+			
+					<div class="container">
+						<div class="row">
+					<div class="col-6" style="float: right; font-size: 20px;"><p>“Uno de los secretos del éxito empresario consiste no en hacer uno mismo el trabajo, sino en reconocer al hombre apropiado para hacerlo”.
+					<br><strong class="a">Andrew Carnegiestr</strong></p>
+					<p>“Debes hacer cosas que realmente sean importantes, pero también debes divertirte porque si no, no tendrás éxito”.
+                    <br><strong>Larry Page</strong></p>		
+                    <p>“No tienes que ser un genio, un visionario o graduado para tener éxito. Todo lo que necesitas es perspectiva y un sueño”.
+                    <br><strong>Michael Dell</strong></p>
+                    <p>“No seas un sabelotodo, en su lugar sé un aprendelotodo”.
+                    <br> <strong>Jeff Weiner</strong></p>
+                    <p>“Compite contra ti mismo, no contra otros”.
+                    <br> <strong> Sophia Amoruso</strong></p>
+                    <p>“Centrarte en algo y hacerlo realmente bien te puede llevar muy lejos”.
+                    <br><strong>Kevin Systrom</strong></p>
+					</div>
+					<div class="col-6" style="float: left;"><img src="Image/1.jpg" alt="CV" style="width:500px;height:590px;"></div>
+
+						</div>
+					</div>
+				
+		</div>
+	</div>
+	
+	<div id="presentacion1">
+		<div class="contenedor2">
+			<h2><a name="ancla-2"><strong>ESTUDIOS</strong></a></h2>
+			
+					<div class="container">
+						<div class="row">
+							<div class="col-6" style="float: left;">  
+									<div>
+			<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/2.jpg" class="d-block w-100" alt="imagen 1" width="75%" height="75%">
+    </div>
+    <div class="carousel-item">
+      <img src="image/3.jpg" class="d-block w-100" alt="imagen 2" width="75%" height="75%">
+    </div>
+    <div class="carousel-item">
+      <img src="image/4.jpg" class="d-block w-100" alt="imagen 3" width="75%" height="75%">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>						
+		</div>
+						    </div>
+
+					
+					<div class="col-6 " style="float: right; font-size: 20px;">
+								<br>
+								<br>
+								<br>
+								<br>
+								<br>
+								<br>
+								<br>
+							<ul style = "list-style-type: none; text-align: center; border:3px #4A235A solid;
+	padding: 10px;
+	margin: 20px;
+	border-radius: 15px;">
+								
+									<li> BACHILLERATO GENERAL OFICAL RAQUEL FLORES GONZALES
+									<li><strong><i>"CERTIFICADO DE NIVEL MEDIO SUPERIOR"</i>  </strong>
+										<br><br>
+									<li> UNIVERSIDAD TECNOLOGICA DE PUEBLA
+									<li><strong><i>"TSU. MECATRONICA ÁREA SISTEMAS DE MANUFACTURA FLEXIBLE" </i> </strong>
+										<br><br>
+									<li>UNIVERSIDAD TECNOLOGICA DE PUEBLA
+									<li><strong><i>"INGENIERIA EN MECATRONICA" </i>  </strong>
+							</ul>
+					</div>
+				
+
+						</div>
+					</div>
+				
+		</div>
+	</div>
+
+<div id="presentacion2">
+		<div class="contenedor2">
+			<h2><a name="ancla-3">PORTAFOLIO</a></h2>
+			
+					<div class="container">
+						<div class="row">
+					<div class="col-6" style="float: right; font-size: 20px;">
+						<div class="card mb-3" style="max-width: 540px;">
+						  <div class="row g-0">
+						    <div class="col-md-4">
+						      <img src="image/monica.png" class="img-fluid rounded-start" alt="moni" style="width:150px; height:210px;" >
+						    </div>
+						    <div class="col-md-8">
+						      <div class="card-body">
+						        <h5 class="card-title" style="color: black;"><strong>PRIMER CUATRIMESTRE</strong></h5>
+						        <p class="card-text" style="color: #212F3C;">El alumno realizará la codificación de los diagramas de flujo o pseudocódigos y resultado de la ejecución de algun proceso.</p>
+						        <p class="card-text" align="right"><small class="text-muted" >Alain Chalieet Petriz Villasis</small></p>
+						      </div>
+						    </div>
+						  </div>
+						</div>
+					</div>
+
+
+					<div class="col-6" style="float: left; font-size: 20px;">
+						<div class="card mb-3" style="max-width: 540px;">
+						  <div class="row g-0">
+						    <div class="col-md-4">
+						      <img src="image/vivian.jpg" class="img-fluid rounded-start" alt="vivi" style="width:150px; height:210px;">
+						    </div>
+						    <div class="col-md-8">
+						      <div class="card-body">
+						        <h5 class="card-title" style="color: black;"><strong>SEGUNDO CUATRIMESTRE</strong></h5>
+						        <p class="card-text" style="color: #212F3C;">Previo a la presentación de su Tarea Integradora, validar que los entregables de tu proyecto con los requisitos mínimos.</p>
+						        <p class="card-text" align="right"><small class="text-muted">Erika Arenas Enríquez</small></p>
+						      </div>
+						    </div>
+						  </div>
+						</div>
+
+					</div>
+
+
+					<div align="center">
+						<div class="col-md-8">
+							<br>
+							<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+							  <div class="carousel-inner">
+							    <div class="carousel-item active">
+							      <img src="image/a.jpg" class="d-block w-100" alt="a" style="width:550px; height:410px;">
+							    </div>
+								    <div class="carousel-item">
+								      <img src="image/b.jpg" class="d-block w-100" alt="b" style="width:550px; height:410px;">
+								    </div>
+									    <div class="carousel-item">
+									      <img src="image/c.jpg" class="d-block w-100" alt="c" style="width:550px; height:410px;">
+									    </div>
+									    <div class="carousel-item">
+									      <img src="image/d.jpg" class="d-block w-100" alt="d" style="width:550px; height:410px;">
+									    </div>
+								    <div class="carousel-item">
+								      <img src="image/e.jpg" class="d-block w-100" alt="e" style="width:550px; height:410px;">
+								    </div>
+							    <div class="carousel-item">
+							      <img src="image/f.jpg" class="d-block w-100" alt="f" style="width:550px; height:410px;">
+							    </div>
+							  </div>
+							</div>
+							<br>
+						</div>
+					</div>
+
+
+						</div>
+					</div>
+
+		</div>
+	</div>
+
+
+		<div id="presentacion3">
+			<div class="contenedor2">
+				<h2><a name="ancla-4">INTERESES</a></h2>
+			
+					<div class="container">
+						<div class="row">
+							
+							<div class="col-md-4">
+								<div class="card" style="width: 18rem;">
+					 				 <img src="image/01.jpg" class="card-img-top" alt="m">
+					  					<div class="card-body">
+					  					  <p class="card-text" style="color: black; text-align: center;"><strong>Música</strong><br>Actualmente uno de mis intereses primordiales es tocar la guitarra de manera informal.</p>
+					 						 </div>
+													</div>
+															</div>
+							<div class="col-md-4">
+								<br>
+									<br>
+								<div class="card" style="width: 18rem;">
+					 				 <img src="image/02.jpg" class="card-img-top" alt="f">
+					  					<div class="card-body">
+					  					  <p class="card-text" style="color: black; text-align: center;"><strong>Fotografía</strong><br>Una de mis mas grandes pasiones es inmortalizar el  momento mediante la fotografía.</p>
+					 						 </div>
+													</div>
+															</div>
+							<div class="col-md-4">
+								<div class="card" style="width: 18rem;">
+					 				 <img src="image/03.jpg" class="card-img-top" alt="c">
+					  					<div class="card-body">
+					  					  <p class="card-text" style="color: black; text-align: center;"><strong>Cocina</strong><br>La alta cosina es uno de mis intereses mas marcados a lo largo del timepo.
+													</div>
+															</div>
+																	</div>
+
+								
+							</div>
+					</div>
+			</div>
+	</div>
+				
+
+		<div class="container" style="text-align: center;">
+			<div class="row">
+				<h2 style="text-align: center; "><a name="ancla-5">CONTACTO</a></h2>
+					<div class="col-md-2"><img src="icons/a.png" style="width:50px; height:50px; text-align: center;"><br>Fernando U. Vallejo</div>
+					<div class="col-md-2"><img src="icons/b.png" style="width:50px; height:50px; text-align: center;"><br>@tbest 221-147-0214</div>
+					<div class="col-md-2"><img src="icons/c.png" style="width:50px; height:50px; text-align: center;"><br>@fernand0717</div>
+					<div class="col-md-2"><img src="icons/d.png" style="width:50px; height:50px; text-align: center;"><br>9 sur 146 <br> col. Reforma Sur</div>
+					<div class="col-md-2"><img src="icons/e.png" style="width:50px; height:50px; text-align: center;"><br>222-16-43-21-74</div>
+					<div class="col-md-2"><img src="icons/f.png" style="width:50px; height:50px; text-align: center;"><br>fer.uri@gmail.com</div>
+			</div>	
+		</div>
+
+
+
+
+
+
+
+																							<footer>
+																								<p>Copyright &copy; 2022 Proyecto kikin CV personal</p>
+																							</footer>
+	
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
